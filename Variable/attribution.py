34cf5ec0b504e1ba8a90ded279972bba0ff1744a
rangademetal <stro3andr3i@gmail.com>
2023-05-04 11:48:24 -0700

    update

--- Variable/attribution.py
+++
@@ -1,9 +0,0 @@
-from Variable.variable_init import Variables
-
-
-class Attribution(Variables):
-    def __init__(self, database):
-        self.database = database
-        Variables.__init__()
-
-    # if Variables.BOOLEAN in [True, False]:
---
+++ Variable/variable.py
@@ -0,0 +1,10 @@
+class Variable:
+    __STR__ = "STR"
+    __INTEGER__ = "INTEGER"
+    __BOOLEAN__ = "BOOLEAN"
+    __FLOAT__ = "FLOAT"
+    __DATE__ = "DATE"
+    __DATETIME__ = "DATETIME"
+    __PRIMARYKEY__ = "PRIMARY KEY"
+    __FOREIGNKEY__ = "FOREIGN KEY"
+    __UNIQUE__ = "UNIQUE"
